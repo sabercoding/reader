86af5d3cbdd16e0a8004ba26d5f50df344471355
shuibo <nm1413@126.com>
2016-12-01 15:26:59 +0800

    配置文件提交

--- Scan.php
+++ Scan.php
@@ -2,19 +2,4 @@
 require_once "Ots.php";
 
 
-//namespace reader;
-//
-//class Scan {
-//    public function scanChapter() {
-//        $chapters_url = 'http://www.ishisetianxia.com/';
-//        $chapter_data = file_get_contents($chapters_url."buxiufanren/");
 $books = (new \reader\Ots())::putChapter('为啥你在这儿', '第三章', '', '第四章', '第二章', time());
-echo 11;
-//    }
-//
-//    public function scanContent($url) {
-//
-//    }
-//
-//}
-//(new Scan())->scanChapter();
